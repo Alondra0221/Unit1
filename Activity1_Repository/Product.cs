46210641ef909c3d3df6207597293a2f9c893046
Alondra0221 <113452015+Alondra0221@users.noreply.github.com>
2022-10-14 11:49:51 -0500

    Add files via upload

---
+++ Activity1_Repository/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Activity1
+{
+
+    public class Product
+    {
+        //Atributes
+
+        public string title { get; set; }
+        public string description { get; set; }
+        public float price { get; set; }
+        public int quantity { get; set; }
+        public int codigo { get; set; }
+        public float total => (price * quantity);
+
+
+        public Product()
+        {
+            //Constructo
+            this.title = "";
+            this.description = "";
+            this.price = 0;
+            this.quantity = 0;
+
+        }
+        
+        
+
+
+    }
+
+
+
+}
+
+
