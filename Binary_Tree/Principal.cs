afb98cd760c2f5a45b9a1cc0065ac81152dc39dc
Alondra0221 <113452015+Alondra0221@users.noreply.github.com>
2022-10-14 11:54:12 -0500

    Add files via upload

---
+++ Binary_Tree/Principal.cs
@@ -0,0 +1,145 @@
+﻿using System;
+namespace Trees
+{
+    internal class Principal
+    {
+
+        private static void Main(string[] args)
+        {
+            Node n1 = new Node();
+            n1.data = 7;
+            n1.parent = null;
+
+            Node n2 = new Node();
+            n2.data = 3;
+            n2.parent = n1;
+
+            Node n3 = new Node();
+            n3.data = 12;
+            n3.parent = n1;
+
+            Node n4 = new Node();
+            n4.data = 1;
+            n4.parent = n2;
+
+            Node n5 = new Node();
+            n5.data = 6;
+            n5.parent = n2;
+
+            Node n6 = new Node();
+            n6.data = 0;
+            n6.parent = n4;
+
+            Node n7 = new Node();
+            n7.data = 2;
+            n7.parent = n4;
+
+            Node n8 = new Node();
+            n8.data = 4;
+            n8.parent = n5;
+
+            Node n9 = new Node();
+            n9.data = 5;
+            n9.parent = n8;
+
+            Node n10 = new Node();
+            n10.data = 9;
+            n10.parent = n3;
+
+            Node n11 = new Node();
+            n11.data = 8;
+            n11.parent = n10;
+
+            Node n12 = new Node();
+            n12.data = 11;
+            n12.parent = n10;
+
+            Node n13 = new Node();
+            n13.data = 10;
+            n13.parent = n12;
+
+            Node n14 = new Node();
+            n14.data = 13;
+            n14.parent = n3;
+
+            Node n18 = new Node();
+            n18.data = 20;
+            n18.parent = n9;
+
+
+            Node n15 = new Node();
+            n15.data = 15;
+            n15.parent = n14;
+
+            Node n16 = new Node();
+            n16.data = 14;
+            n16.parent = n15;
+
+
+
+            Tree t1 = new Tree();
+            t1.addElement(n1, null);
+            t1.addElement(n2, n2.parent);
+            t1.addElement(n3, n3.parent);
+            t1.addElement(n4, n4.parent);
+            t1.addElement(n5, n5.parent);
+            t1.addElement(n6, n6.parent);
+            t1.addElement(n7, n7.parent);
+            t1.addElement(n8, n8.parent);
+            t1.addElement(n9, n9.parent);
+            t1.addElement(n10, n10.parent);
+            t1.addElement(n11, n11.parent);
+            t1.addElement(n12, n12.parent);
+            t1.addElement(n13, n13.parent);
+            t1.addElement(n14, n14.parent);
+            t1.addElement(n15, n15.parent);
+            t1.addElement(n16, n16.parent);
+            t1.addElement(n18, n18.parent);
+
+            //t1.printTree(t1.root, 0);
+
+           
+
+            //t1.removeElement(3);
+
+            
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            t1.printTree(t1.root, 0);
+
+            //t1.BuscarInfo(3);
+
+            //t1.printTree(t1.root, 0);
+
+
+
+            //Console.WriteLine("\nInOrder\n");
+            //t1.inOrder(t1.root);
+
+            //Console.WriteLine();
+
+            //Console.WriteLine("\nPreOrder\n");
+            //t1.preOrder(t1.root);
+
+            //Console.WriteLine();
+
+            //Console.WriteLine("\nPostOrder\n");
+            //t1.postOrder(t1.root);
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
+
---
+++ Binary_Tree/Tree.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.Intrinsics.X86;
+
+namespace Trees
+{
+    public class Tree
+    {
+        //Atributes
+
+        public Node? root;
+        public Node encontrado;
+        private int level;
+        
+        
+        
+
+        public Tree()
+        {
+            //Constructor
+            this.root = null;
+            
+
+        }
+        // Buscar 
+        public void addElement(Node Element, Node? Parent)
+        {
+            
+            if(Parent == null)
+            {
+                root = Element;
+                return;
+            }
+
+            buscar(Element.data);
+
+            if (encontrado != null)
+            {
+                Console.WriteLine("You cant add the data " + Element.data + " because is already storage");
+            }
+
+            else if( Parent.left == null)
+            {
+                Parent.left = Element;
+                return;
+            }
+
+            else if( Parent.right== null)
+            {
+                Parent.right = Element;
+                return;
+            }
+
+            else if (Parent.left != null && Parent.right != null)
+            {
+                Console.WriteLine("You can't add the data " + Element.data + " because the node is completed");
+            }
+
+                
+        }
+
+        //Buscar
+        public void BuscarInfo(int dato)
+        {
+            buscar(dato);
+
+            if (encontrado!= null)
+            {
+
+                Console.WriteLine("The nodo was found: " + " \nIt's data is: " + encontrado.data);
+
+                if (encontrado .parent != null) {
+                    Console.WriteLine("The parent is: " + encontrado.parent.data);
+                }
+                else
+                {
+                    Console.WriteLine("It doesn't has parent");
+                }
+                if (encontrado.left != null)
+                {
+                    Console.WriteLine("The left child is: " + encontrado.left.data);
+                }
+                else
+                {
+                    Console.WriteLine("It doesn't has left child");
+                }
+
+                if (encontrado.right != null)
+                {
+                    Console.WriteLine("The left child is: " + encontrado.right.data);
+                }
+                else
+                {
+                    Console.WriteLine("It doesn't has right child");
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("The node wasn't found");
+            }
+
+        }
+
+    
+        public void buscar(int dato)
+        {
+            bool flag = false;
+            Node? aux = root;
+
+            if (aux == null)
+            {
+                return;
+            }
+            search(aux, dato, flag);
+            
+
+        }
+        private void search(Node? aux, int dato, bool flag)
+        {
+
+            
+            if (aux.data != dato && flag == false)
+            {
+                if (aux.left != null)
+                {
+                    
+                    aux = aux.left;
+                    search(aux, dato, flag);
+                    aux = aux.parent;
+                }
+
+                if (aux.right != null)
+                {
+                    aux = aux.right;
+                    search(aux, dato, flag);
+                    aux = aux.parent;
+                }
+
+                
+                return;
+ 
+            }
+            else
+            {
+                
+                flag = true;
+                encontrado = aux;
+                return;
+
+            }
+
+                  
+
+        }
+
+       
+        public void printTree(Node? r, int c) {
+
+            
+            if (r == null) // We validate if the tree is null
+            {
+                return;
+            }
+            
+
+
+            if(r.right != null)
+            {
+                
+                printTree(r.right, c+1);
+   
+            }
+
+            for (int i=0; i< c; i++) //I made this for because it's necesary to leave space btween each node.
+                {   
+                    Console.Write("         ");
+                }
+
+            
+            Console.WriteLine( r.data);
+            
+
+            if (r.left != null)
+            {
+                
+                printTree(r.left, c + 1);
+                
+            }  
+
+            
+        }
+
+
+        //Buscar
+        public void removeElement(int dato) {
+
+            buscar(dato);
+
+           
+
+            if ( encontrado != null)
+            {
+
+                if (encontrado.parent.left.data == encontrado.data)
+
+                {
+                    encontrado.parent.left = null;
+                    
+                }
+
+                else
+                {
+                    encontrado.parent.right = null;
+                    
+                }
+                
+            }
+
+            else
+            {
+                Console.WriteLine("The data is not storage");
+            }
+
+        }
+
+        public void inOrder(Node? r)
+        {
+
+            if (r != null)
+            {
+                inOrder(r.left);
+                Console.Write(r.data + " ");
+                inOrder(r.right);
+
+            }
+
+        }
+
+        public void preOrder( Node? r)
+        {
+            if (r!= null)
+            {
+                Console.Write(r.data + " ");
+                preOrder(r.left);
+                preOrder(r.right);
+
+            }
+        }
+
+        public void postOrder(Node? r)
+        {
+            if (r!= null)
+            {
+                postOrder(r.left);
+                postOrder(r.right);
+                Console.Write(r.data + " ");
+            }
+        }
+    }
+}
+
